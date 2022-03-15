273d5641d23d86254e9ea3e20f98414af432d460
InnocentSinEternalSenia <92751077+InnocentSinEternalSenia@users.noreply.github.com>
2022-03-15 20:16:28 +0100

    i don't get it, what are summaries for

---
+++ day2/AdventOfCode2/AdventOfCode2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            const string input = @"forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2";
+            string[] lines = input.Split('\n');
+
+            int horizontalPosition = 0;
+            int depth = 0;
+            int aim = 0;
+
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string[] lineSplit = lines[i].Split(' ');
+                //Console.WriteLine(lineSplit[1]);
+
+                switch (lineSplit[0])
+                {
+                    case "forward":
+                        horizontalPosition += int.Parse(lineSplit[1]);
+                        depth += int.Parse(lineSplit[1]) * aim;
+                        break;
+                    case "down":
+                        aim += int.Parse(lineSplit[1]); //1* depth += int.Parse(lineSplit[1]);
+                        break;
+                    case "up":
+                        aim -= int.Parse(lineSplit[1]); //1* depth -= int.Parse(lineSplit[1]);
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+
+            int result = depth * horizontalPosition;
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ day3/help/help/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace help
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = @"00100
+11110
+10110
+10111
+10101
+01111
+00111
+11100
+10000
+11001
+00010
+01010";
+
+            string[] lines = input.Split('\n');
+
+            string result = "";
+
+            for (int j = 0; j < 5; j++)
+            {
+                int zeros = 0;
+                int ones = 0;
+
+                for (int i = 0; i < lines.Length; i++)
+                {
+
+                    char[] line = lines[i].ToCharArray();
+
+                    if (line[j] == '0')
+                    {
+                        zeros++;
+                    }
+                    else
+                    {
+                        ones++;
+                    }
+
+
+                }
+
+                if (zeros > ones)
+                {
+                    result += "0";
+                }
+                else
+                {
+                    result += "1";
+                }
+            }
+
+            Console.WriteLine(result);
+
+            string reversedResult = "";
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                if (result.ToCharArray()[i] == '0')
+                {
+                    reversedResult += "1";
+                }
+                else
+                {
+                    reversedResult += "0";
+                }
+            }
+
+            Console.WriteLine(reversedResult);
+
+            int resultToDecimal = Convert.ToInt32(result, 2); //https://stackoverflow.com/questions/1961599/how-to-convert-binary-to-decimal
+            int reversedResultToDecimal = Convert.ToInt32(reversedResult, 2);
+
+            Console.WriteLine(resultToDecimal * reversedResultToDecimal);
+            
+
+
+            Console.ReadLine();
+        }
+    }
+}
